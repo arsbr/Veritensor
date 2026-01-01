8e9cb50e2b5cbd9252c0925be0c35363cb7f5bcd
ArseniiBrazhnyk <95613764+ArseniiBrazhnyk@users.noreply.github.com>
2026-01-01 16:55:55 +0100

    Update main.py

--- src/veritensor/cli/main.py
+++ src/veritensor/cli/main.py
@@ -280,7 +280,7 @@ def keygen(output_prefix: str = "veritensor"):
 @app.command()
 def version():
     """Show version info."""
-    console.print("Veritensor v1.0.1 (Community Edition)")
+    console.print("Veritensor v1.0.2 (Community Edition)")
 
 
 if __name__ == "__main__":
