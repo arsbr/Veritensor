2b8184a9eabdf7b5297ec97b8ff07c2d2a5938bd
arsbr <95613764+arsbr@users.noreply.github.com>
2026-04-20 23:33:23 +0200

    Update safe_zip.py

--- src/veritensor/core/safe_zip.py
+++ src/veritensor/core/safe_zip.py
@@ -1,4 +1,4 @@
-
+# Veritensor Apache 2.0
 import zipfile
 import logging
 
@@ -11,7 +11,6 @@ class SafeZipReader:
     # Limits: 2 GB decompressed, compression ratio 100x
     MAX_UNZIPPED_SIZE = 2 * 1024 * 1024 * 1024 
     MAX_RATIO = 10
-    MIN_SIZE_FOR_RATIO = 10 * 1024 * 1024 
 
     @staticmethod
     def validate(zfile: zipfile.ZipFile):
@@ -25,8 +24,7 @@ class SafeZipReader:
                 raise ZipBombError(f"Filename too long in zip: {info.filename[:50]}...")
 
             # Checking the compression ratio
-            #if info.file_size > 0 and info.compress_size > 0:
-            if info.file_size > SafeZipReader.MIN_SIZE_FOR_RATIO and info.compress_size > 0:
+            if info.file_size > 0 and info.compress_size > 0:
                 ratio = info.file_size / info.compress_size
                 if ratio > SafeZipReader.MAX_RATIO:
                     raise ZipBombError(f"Zip Bomb detected! Ratio {ratio:.1f}x exceeds limit.")
